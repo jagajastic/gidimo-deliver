1e7bc58edf4143e1585ffd6cce2e5596529efb6d
Ibrahim Joseph <josephibrahi@gmail.com>
2019-10-04 11:18:07 +0100

    add show handburger method

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  selector: "app-nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styleUrls: ["./nav-bar.component.css"]
 })
 export class NavBarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  clickHandBurger(show: boolean) {
+    const el = document.querySelector(".main-nav-bar");
+    el.classList.toggle("show-menu");
   }
-
 }

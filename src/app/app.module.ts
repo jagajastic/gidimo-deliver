9f5e8d7af9bfa70dce933af117d732e0ba992128
Ibrahim Joseph <josephibrahi@gmail.com>
2019-10-04 05:54:19 +0100

    add popular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { TestimonyCardComponent } from './testimony-card/testimony-card.component';
 import { DownloadComponentComponent } from './download-component/download-component.component';
+import { PopularRestaurantsComponent } from './popular-restaurants/popular-restaurants.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     TestimonyCardComponent,
-    DownloadComponentComponent
+    DownloadComponentComponent,
+    PopularRestaurantsComponent
   ],
   imports: [
     BrowserModule,

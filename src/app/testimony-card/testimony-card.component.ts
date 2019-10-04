41504987ef354206f5c96b240434278b8899db99
Ibrahim Joseph <josephibrahi@gmail.com>
2019-10-04 05:44:53 +0100

    add testimony component and move testimony section from app root to testimony  component

---
+++ src/app/testimony-card/testimony-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testimony-card',
+  templateUrl: './testimony-card.component.html',
+  styleUrls: ['./testimony-card.component.css']
+})
+export class TestimonyCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

a79973aba0858f0a3e1cd3a301a359e33f4212c6
Ibrahim Joseph <josephibrahi@gmail.com>
2019-10-04 21:14:40 +0100

    mock data for popula and how it work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,28 +28,48 @@ export class AppComponent {
       }
     ]
   };
-  // popular: object = {
-  //   title: "Popular Restaurants",
-  //   view: "false",
-  //   restaurants: [
-  //     {
-  //       title: "Restaurant A",
-  //       rate: 3
-  //     },
-  //     {
-  //       title: "Restaurant B",
-  //       rate: 3
-  //     },
-  //     {
-  //       title: "Restaurant C",
-  //       rate: 3
-  //     },
-  //     {
-  //       title: "Restaurant D",
-  //       rate: 3
-  //     }
-  //   ]
-  // };
+  popular = {
+    title: "Popular Restaurants",
+    view: false,
+    restaurants: [
+      {
+        title: "Restaurant A",
+        rate: 3
+      },
+      {
+        title: "Restaurant B",
+        rate: 3
+      },
+      {
+        title: "Restaurant C",
+        rate: 3
+      },
+      {
+        title: "Restaurant D",
+        rate: 3
+      }
+    ]
+  };
 
+  howItWork = {
+    title: "How It Works",
+    steps: [
+      {
+        title: "Select Your Restaurant",
+        context:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, quibusdam! Omnis et, id nesciunt laboriosam quae culpa eos sed enim, ut quas error ducimus unde, quidem quaerat hic blanditiis. Totam."
+      },
+      {
+        title: "Select Your Desired Meal",
+        context:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, quibusdam! Omnis et, id nesciunt laboriosam quae culpa eos sed enim, ut quas error ducimus unde, quidem quaerat hic blanditiis. Totam."
+      },
+      {
+        title: "Get It Deliver Fast",
+        context:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, quibusdam! Omnis et, id nesciunt laboriosam quae culpa eos sed enim, ut quas error ducimus unde, quidem quaerat hic blanditiis. Totam."
+      }
+    ]
+  };
   title = "gidimo-deliver";
 }
